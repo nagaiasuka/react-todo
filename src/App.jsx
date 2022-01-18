9982a7a0b764fff342f5ed3141b9305d9f474feb
Nagai Asuka <87313557+nagaiasuka@users.noreply.github.com>
2022-01-18 22:02:32 +0900

    見た目作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,47 @@
+import React, { useState } from "react";
 import "./styles.css";
 
-export default function App() {
+export const App = () => {
+  const [incompleteTodos, setIncompleteTodos] = useState([
+    "ああああ",
+    "いいいい"
+  ]);
+  const [completeTodos, setCompleteTodos] = useState(["うううう", "ええええ"]);
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <>
+      <div className="input-area">
+        <input placeholder="ToDoを入力" />
+        <button>追加</button>
+      </div>
+
+      <div className="incomplete-area">
+        <p className="title">未完了のToDo</p>
+        <ul>
+          {incompleteTodos.map((todo) => {
+            return (
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button>完了</button>
+                <button>削除</button>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className="complete-area">
+        <p className="title">完了のToDo</p>
+        <ul>
+          {completeTodos.map((todo) => {
+            return (
+              <div className="list-row">
+                <li>{todo}</li>
+                <button>戻す</button>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+    </>
   );
-}
+};
