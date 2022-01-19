ec23793eb6312d66c5343254f61980c3acda933f
Nagai Asuka <87313557+nagaiasuka@users.noreply.github.com>
2022-01-19 20:00:23 +0900

    削除機能実装

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,13 @@ export const App = () => {
     setTodoText("");
   };
 
+  //削除機能
+  const onClickDelete = (index) => {
+    const newTodos = [...incompleteTodos];
+    newTodos.splice(index, 1);
+    setIncompleteTodos(newTodos);
+  };
+
   return (
     <>
       <div className="input-area">
@@ -34,12 +41,12 @@ export const App = () => {
       <div className="incomplete-area">
         <p className="title">未完了のToDo</p>
         <ul>
-          {incompleteTodos.map((todo) => {
+          {incompleteTodos.map((todo, index) => {
             return (
               <div key={todo} className="list-row">
                 <li>{todo}</li>
                 <button>完了</button>
-                <button>削除</button>
+                <button onClick={() => onClickDelete(index)}>削除</button>
               </div>
             );
           })}
